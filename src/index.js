b56689c2a7b778fc2c2db92f90008b2d424ddc39
mohanraj-10decoders <mohanraj.l@10decoders.in>
2022-07-19 15:23:20 +0530

    With comments

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Suspense, lazy } from 'react';
 
-import Artists from './Artists';
+// import Artists from './Artists';
 
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className='App'>
+//         <Artists />
+//       </div>
+//     );
+//   }
+// }
+const Artists = lazy(() => import('./Artists'));
+
+const styles = {
+  color: 'red',
+  fontSize: '2rem',
+  width: '100%',
+  height: '100vh',
+  textAlign: 'center',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
 class App extends React.Component {
-    render(){
-        return(
-            <div className="App">
-            <Artists />
+  render() {
+    return (
+      <div className='App'>
+        <Suspense
+          fallback={
+            <div className='loader' style={styles}>
+              Loading...
             </div>
-        );
-    }
+          }
+        >
+          <Artists />
+        </Suspense>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
